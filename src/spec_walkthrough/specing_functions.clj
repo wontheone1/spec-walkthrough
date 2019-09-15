bdb1f8ebba1d11d4c6f2e8108e7f48381cec2e39
Won Seob Seo <won.seob.seo@zalando.fi>
2019-09-15 17:52:27 +0300

    Spec'ing functions

---
+++ src/spec_walkthrough/specing_functions.clj
@@ -0,0 +1,17 @@
+(ns spec-walkthrough.specing-functions
+  (:require
+    [clojure.spec.alpha :as s]))
+
+(defn ranged-rand
+  "Returns random int in range start <= rand < end"
+  [start end]
+  (+ start (long (rand (- end start)))))
+
+(s/fdef ranged-rand
+        :args (s/and (s/cat :start int? :end int?)
+                     #(< (:start %) (:end %)))
+        :ret int?
+        :fn (s/and #(>= (:ret %) (-> % :args :start))
+                   #(< (:ret %) (-> % :args :end))))
+
+; (doc ranged-rand)
